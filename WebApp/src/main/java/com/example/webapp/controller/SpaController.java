149615f3d74e29760b7295b8fffc7914967ca391
Amjith krishnan <amjithkrishnanuj.com>
2026-03-06 23:32:17 +0000

    check

--- WebApp/src/main/java/com/example/webapp/controller/SpaController.java
+++ WebApp/src/main/java/com/example/webapp/controller/SpaController.java
@@ -1,13 +1,18 @@
 package com.example.webapp.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SpaController {
     
-    @GetMapping(value = {"/"})
-    public String index() {
+    @GetMapping(value = {"/{path:[^\\.]*}"})
+    public String redirect(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        if (path.startsWith("/api/")) {
+            return null;
+        }
         return "forward:/index.html";
     }
 }
